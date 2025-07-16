cbfee00fdc21be233d1cb63b7977189d0a7b1937
bisckoot <100939251+amish-tufail@users.noreply.github.com>
2025-07-16 23:24:26 +1000

    Async Await

---
+++ ConcurrencyPractice/DownloadWithAsync.swift
@@ -0,0 +1,18 @@
+//
+//  DownloadWithAsync.swift
+//  ConcurrencyPractice
+//
+//  Created by Amish Tufail on 16/07/2025.
+//
+
+import SwiftUI
+
+struct DownloadWithAsync: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DownloadWithAsync()
+}
