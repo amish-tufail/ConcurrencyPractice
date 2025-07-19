7b42458cd4b4f6dea72b90c909a7aadf342ce3bd
bisckoot <100939251+amish-tufail@users.noreply.github.com>
2025-07-19 20:51:27 +1000

    Task group

---
+++ ConcurrencyPractice/TaskGroupBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  TaskGroupBootcamp.swift
+//  ConcurrencyPractice
+//
+//  Created by Amish Tufail on 19/07/2025.
+//
+
+import SwiftUI
+
+struct TaskGroupBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TaskGroupBootcamp()
+}
